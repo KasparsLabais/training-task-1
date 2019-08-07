7fe40ca7094d1c4dc8a5ebaeb796f26a84321304
demacc <53468355+demacc@users.noreply.github.com>
2019-08-07 09:57:13 +0100

    new html layout

--- index.php
+++ index.php
@@ -4,32 +4,62 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="css/admin.css" rel="stylesheet" type="text/css"
-  <!--  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script */ -->
+    <!--  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script */ -->
 </head>
 
 <body>
+<div class="container">
+    <div id="rampant-nav-bar">
+        <h1>RampantTV</h1>
+        <nav>
+            <ul>
+                <li><a href="#">CHANNELS</a></li>
+                <li><a href="#">FORUM</a></li>
+                <li><a href="#">VIDEOS</a></li>
+                <li><a href="#">SCHEDULE</a></li>
+                <li><a href="#">TRENDING</a></li>
+                <li><a href="#">SOCIAL</a></li>
+                <li><a href="#">SHOWS</a></li>
+                <li><a href="#">ADMIN</a></li>
+            </ul>
+        </nav>
 
-<div style = "background-color: black; width: 30%; height: 60%; color:white;">
-    <h4>Admin Panel</h4>
+    </div>
+    <div class="container">
+        <id="left-nav-bar" style:"overflow-auto">
+    </div>
 
-    <div><p>Admin Info</p>
-        <ul>
-            <li>Admin Name</li>
-            <li>Role</li>
-        </ul>
+    <div id="admin-info">
+        <h1>Admin Info</h1>
+        <li>Admin Name</li>
+        <li>Role</li>
+    </div>
+
+    <div id="admin-panel"
+</div>
+<ul>
+    <li>Marketing
+        <ul>Promo Manager</ul>
+    </li>
+    <li>Feeds
+        <ul>Feeds Management</ul>
+        <ul>Add New Feed</ul>
+    </li>
+    <li>Users/Sales Stats
+        <ul>Generate Mail Listr</ul>
+        <ul>Unsubscribe Users</ul>
+    </li>
+
+</ul>
+</ul>
 </div>
 
-    <div style = "background-color: pink; width: 30%; height: 60%; color:white;">
-        <div> <h4> Admin Control Div </h4>
-            <ul>
-                <li>User Editor</li>
-                <li>Option 2</li>
-                <li>Option 3</li>
-                <li>Option 4</li>
-            </ul>
-        </div>
+<div class="container">
+    <div id="main-body">
+    </div>
+
 
 </body>
 
