5f6c20f4286bfd929b1ba76d9767c7521413b359
Danni <dmclaren@firestormmedia.tv>
2019-08-07 15:50:35 +0100

    changed admin panel layout

--- index.php
+++ index.php
@@ -6,10 +6,11 @@
     <link href="css/admin.css" rel="stylesheet" type="text/css"
     <!--  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
       <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script */ -->
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script> -->
 </head>
 
 <body>
+
 <div class="container">
     <div id="rampant-nav-bar">
         <h1>RampantTV</h1>
@@ -27,97 +28,154 @@
         </nav>
 
     </div>
-
-    <div class="container">
-        <id="left-nav-bar" style:"overflow-auto">
-    </div>
-
-    <div id="admin-info">
-        <h1>Admin Info</h1>
-        <li>Admin Name</li>
-        <li>Role</li>
-    </div>
-
-    <div id="admin-panel"
 </div>
-<ul>
-    <li>Marketing
-        <ul>Promo Manager</ul>
-    </li>
-    <li>Feeds
-        <ul>Feeds Management</ul>
-        <ul>Add New Feed</ul>
-    </li>
-    <li>Users/Sales Stats
-        <ul>Generate Mail Listr</ul>
-        <ul>Unsubscribe Users</ul>
-    </li>
-    <li>User Stats
-        <ul>PRS Callers Free Minutes Stats</ul>
-        <ul>Inactive Users</ul>
-        <ul>Webcam Stats</ul>
-        <ul>Management Stats</ul>
-        <ul>Topup Stats</ul>
-        <ul>Rampant VR Stats</ul>
-        <ul>Roku Stats</ul>
-        <ul>Lottery Stats</ul>
-    </li>
-    <li>Servers
-        <ul>MSioF</ul>
-        <ul>Pingdom</ul>
-        <ul>Portal</ul>
-    </li>
-    <li>Whitelist
-        <ul>Manage IPs</ul>
-    </li>
-    <li>Helpers
-        <ul>Clear Forgotton Password IP Cache</ul>
-        <ul>Clear any Memcache Key</ul>
-        <ul>User Editor</ul>
-        <ul>Show Creator</ul>
-    </li>
-    <li>Force Default Stream
-        Force the default stream id to
-        <div
-        <form>
-            <input type="number" name="from-value" min="1" max="9999999">
-            for
-            <input type="number" name="to-value" min="1" max="9999999">
-            seconds
-            <button type="submit">Submit</button>
-        </form>
-    </li>
-    </div>
-    <li>Localhost Helpers
-        <ul>Concat/Min JS & CSS</ul>
-    </li>
-    </div>
 
-    <div class="container">
-        <div id="top-half-body">
-            <div id="main-user-details">
-                <li>
-                    <button type="button">Impersonate</button>
-                </li>
-            </div>
-            <div id="secondary-user-details">
-                <li>
-                    <ul>Status</ul>
-                    <ul>CCIVRID</ul>
-                    <ul>VIP LEVEL
-                        <button type="reset">Reset VIP</button>
-                        <button type="submit">Give VIP Status</button>
-                    </ul>
-                    <ul>Email</ul>
-                    <ul>dirtychatUserID</ul>
-                    <button type="submit">Change Password</button>
-                </li>
-                <div class="container">
-                    <div id="bottom-half-body>
+<div class="container">
+    <!-- LEFT NAV BAR 30% -->
+    <div id="admin-nav-bar">
+        <div id="admin-info">
+            <h1>Admin Info</h1>
+            <li>Admin Name</li>
+            <li>Role</li>
+        </div>
 
+        <div id="admin-panel">
+        </div>
     </div>
+    <!-- END LEFT NAV BAR 30% -->
+
+    <!-- MAIN INFO CONTAINER 70% -->
+
+    <div id="admin-info-container">
+    </div>
+    <!-- END INFO CONTAINER 70% -->
+</div>
+<div id="admin-panel-marketing">
+    <h1 class="admin-panel-subheader">
+        Marketing
+    </h1>
+    <ul>
+        <li><a href="#">Promo Manager</a></li>
+    </ul>
+</div>
+
+<div id="admin-panel-feeds"><
+    <h1 class="admin-panel-subheader">
+        Feeds
+    </h1>
+    <ul>
+        <li><a href="#">Feeds Management</a></li>
+        <li><a href="#">Add New Feeds</a></li>
+    </ul>
+</div>
+
+<div id="admin-panel-usersales-stats">
+    <h1 class="admin-panel-subheader">
+        User/Sales Stats
+    </h1>
+    <ul>
+        <li><a href="#">Generate Mail List</a></li>
+        <li><a href="#">Unsubscribe Users</a></li>
+    </ul>
+</div>
+
+<div id="admin-panel-user-stats">
+    <h1 class="admin-panel-subheader">
+        User Stats
+    </h1>
+    <ul>
+        <li><a href="#">PRS Callers Free Mins Stats</a></li>
+        <li><a href="#">Inactive Users</a></li>
+        <li><a href="#">Webcam Stats</a></li>
+        <li><a href="#">Management Stats</a></li>
+        <li><a href="#">Topup Stats</a></li>
+        <li><a href="#">Rampant VR Stats</a></li>
+        <li><a href="#">Roku Stats</a></li>
+        <li><a href="#">Lottery Stats</a></li>
+    </ul>
+</div>
+
+<div id="admin-panel-servers">
+    <h1 class="admin-panel-subheader">
+        Servers
+    </h1>
+    <ul>
+        <li><a href="#">MSioF</a></li>
+        <li><a href="#">Pingdom</a></li>
+        <li><a href="#">Portal</a></li>
+    </ul>
+</div>
+
+<div id="admin-panel-whitelist">
+    <h1 class="admin-panel-subheader">
+        WhiteList
+    </h1>
+    <ul>
+        <li><a href="#">Manage IPs</a></li>
+    </ul>
+</div>
+
+<div id="admin-panel-helpers">
+    <h1 class="admin-panel-subheader">
+        Helpers
+    </h1>
+    <ul>
+        <li><a href="#">Clear Forgotten Password IP Cache</a></li>
+        <li><a href="#">Clear any Memcache Key</a></li>
+        <li><a href="#">User Editor</a></li>
+        <li><a href="#">Show Creator</a></li>
+    </ul>
+</div>
+
+<div id="admin-panel-force-default-stream">
+    <h1 class="admin-panel-subheader">
+        Force Default Stream
+    </h1>
+    <form>
+        <p>Force the default stream id to</p>
+        <input type="number" name="from-value" min="1" max="99999">
+        for
+        <input type="number" name="to-value" min="1" max="99999">
+        <button type="submit" value="submit">Set</button>
+    </form>
+</div>
+<div id="admin-panel-localhost-helpers">
+    <h1>
+        Localhost Helpers
+    </h1>
+    <ul>
+        <li><a href="#">Concan/Min JS & CSS</a></li>
+    </ul>
+</div>
+
+
+<div class="container">
+    <div id="top-half-body">
+        <div id="main-user-details">
+
+            <button type="button">Impersonate</button>
+        </div>
+
+
+        <div id="secondary-user-details">
+            <ul>
+                <li>Status</li>
+                <li>CCIVRID</li>
+                <li>VIP LEVEL</li>
+                <button type="reset">Reset VIP</button>
+                <button type="submit">Give VIP Status</button>
+            </ul>
+        </div>
+            <ul>Email</ul>
+            <ul>dirtychatUserID</ul>
+            <button type="submit">Change Password</button>
+            </ul>
+
+
+            <div class="container">
+                <div id="bottom-half-body">
+
+</div>
 
 </body>
-
-
-</html>
+</html></div>
