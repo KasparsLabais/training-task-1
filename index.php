b5867fe4f0488f47e0b1b8c1ea483eea5fcb4fd1
Danni <dmclaren@firestormmedia.tv>
2019-08-15 15:30:28 +0100

    it's FUCKED but i'm fixing it

--- index.php
+++ index.php
@@ -9,6 +9,8 @@
     <!--  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
       <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
       <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script> -->
+    <<script type="text/javascript" src="whatthefuck.js"></script>
+
 </head>
 
 <body style="margin: 0px; border: 0px;">
@@ -39,17 +41,97 @@
 
     </div>
 </div>
+
+<div id="main-container" style="width:100%; background-color:green;">
+<div class="left-div">
+    <div class="small-admin-panel" style="width:30%;">
+        <div class="small-admin-text">
+            <h4>Danielle</h4>
+            <p>Admin</p>
+        </div>
+        <div class="small-admin-image">
+        </div>
+    </div>
+    <div class="small-admin-sidebar">
+        <h3 class="item-father-1"><i class="fas fa-ad"></i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Marketing</h3>
+        <ul class="item-child-1">
+            <li>Promo Manager</li>
+        </ul>
+        <h3 style="margin-top: 0;"><i class="fas fa-marker"></i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Feeds</h3>
+        <ul>
+            <li>Feeds Management</li>
+            <li>Add New Feed</li>
+        </ul>
+        <h3 style="margin-top: 0;"><i class="fas fa-marker"></i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;User/Sales Stats</h3>
+        <ul>
+            <li>Generate Mail List</li>
+            <li>Unsubscribe Users</li>
+        </ul>
+        <h3 style="margin-top: 0;"><i class="fas fa-marker"></i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;User Stats</h3>
+        <ul>
+            <li><a href="#">PRS Callers Free Mins Stats</a></li>
+            <li><a href="#">Inactive Users</a></li>
+            <li><a href="#">Webcam Stats</a></li>
+            <li><a href="#">Management Stats</a></li>
+            <li><a href="#">Topup Stats</a></li>
+            <li><a href="#">Rampant VR Stats</a></li>
+            <li><a href="#">Roku Stats</a></li>
+            <li><a href="#">Lottery Stats</a></li>
+        </ul>
+        <h3 style="margin-top: 0;"><i class="fas fa-marker"></i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Servers</h3>
+        <ul>
+            <li><a href="#">MSioF</a></li>
+            <li><a href="#">Pingdom</a></li>
+            <li><a href="#">Portal</a></li>
+        </ul>
+        <h3 style="margin-top: 0;"><i class="fas fa-marker"></i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;WhiteList</h3>
+        <ul>
+            <li><a href="#">Manage IPs</a></li>
+        </ul>
+        <h3 style="margin-top: 0;"><i class="fas fa-marker"></i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Helpers</h3>
+        <ul>
+            <li><a href="#">Clear Forgotten Password IP Cache</a></li>
+            <li><a href="#">Clear any Memcache Key</a></li>
+            <li><a href="#">User Editor</a></li>
+            <li><a href="#">Show Creator</a></li>
+        </ul>
+        <h3 style="margin-top: 0;"><i class="fas fa-marker"></i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Force Default Stream</h3>
+        <form>
+            <p>Force the default stream id to</p>
+            <input type="number" name="from-value" min="1" max="99999"> for
+            <input type="number" name="to-value" min="1" max="99999">
+            <button type="submit" value="submit">Set</button>
+        </form>
+        <h3 style="margin-top: 0;"><i class="fas fa-marker"></i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Localhost Helpers</h3>
+        <ul>
+            <li><a href="#">Concan/Min JS & CSS</a></li>
+        </ul>
+    </div>
+</div>
+</div>
+
+
+
+<div class="right-div">
+
+</div>
+<!--
 <div id="container" style="width:100%; background-color:green;">
-    <!-- LEFT NAV BAR 30% -->
+    <!-- LEFT NAV BAR 30%
     <div id="admin-nav-bar">
         <div id="admin-info">
-            <img src="http://3.bp.blogspot.com/-ku5soa8KViM/TkA22DK8fdI/AAAAAAAAAoo/dQ0Fqlv1_bo/s1600/dogs-wearing-wigs-0011.jpg" alt="admin-profile-pic" style="border-radius: 50%; height: 75px; float: right; padding: 10px 10px 10px 10px;">
+            <img src="http://3.bp.blogspot.com/-ku5soa8KViM/TkA22DK8fdI/AAAAAAAAAoo/dQ0Fqlv1_bo/s1600/dogs-wearing-wigs-0011.jpg"
+                 alt="admin-profile-pic"
+                 style="border-radius: 50%; height: 75px; float: right; padding: 10px 10px 10px 10px;">
             <div id="admin-role-name">
-            <li style="font-size: 20px">Danielle McLaren</li>
-            <li style="font-size: 20px; color: rgba(0,0,0,0.5)">Administrator</li>
+                <li style="font-size: 20px; color: #63B0CD;">Danielle McLaren</li>
+                <li style="font-size: 20px; color: white;">Administrator</li>
             </div>
         </div>
+
+
         <div id="admin-panel-controls">
+
             <div id="admin-panel-marketing">
                 <h1 class="admin-panel-subheader">
                     Marketing
@@ -57,98 +139,97 @@
                 <ul>
                     <li><a href="#">Promo Manager</a></li>
                 </ul>
-            </div>
 
-            <div id="admin-panel-feeds">
-                <h1 class="admin-panel-subheader">
-                    Feeds
-                </h1>
-                <ul>
-                    <li><a href="#">Feeds Management</a></li>
-                    <li><a href="#">Add New Feeds</a></li>
-                </ul>
-            </div>
+        <div id="admin-panel-feeds">
+            <h1 class="admin-panel-subheader">
+                Feeds
+            </h1>
+            <ul>
+                <li><a href="#">Feeds Management</a></li>
+                <li><a href="#">Add New Feeds</a></li>
+            </ul>
+        </div>
 
-            <div id="admin-panel-usersales-stats">
-                <h1 class="admin-panel-subheader">
-                    User/Sales Stats
-                </h1>
-                <ul>
-                    <li><a href="#">Generate Mail List</a></li>
-                    <li><a href="#">Unsubscribe Users</a></li>
-                </ul>
-            </div>
+        <div id="admin-panel-usersales-stats">
+            <h1 class="admin-panel-subheader">
+                User/Sales Stats
+            </h1>
+            <ul>
+                <li><a href="#">Generate Mail List</a></li>
+                <li><a href="#">Unsubscribe Users</a></li>
+            </ul>
+        </div>
 
-            <div id="admin-panel-user-stats">
-                <h1 class="admin-panel-subheader">
-                    User Stats
-                </h1>
-                <ul>
-                    <li><a href="#">PRS Callers Free Mins Stats</a></li>
-                    <li><a href="#">Inactive Users</a></li>
-                    <li><a href="#">Webcam Stats</a></li>
-                    <li><a href="#">Management Stats</a></li>
-                    <li><a href="#">Topup Stats</a></li>
-                    <li><a href="#">Rampant VR Stats</a></li>
-                    <li><a href="#">Roku Stats</a></li>
-                    <li><a href="#">Lottery Stats</a></li>
-                </ul>
-            </div>
+        <div id="admin-panel-user-stats">
+            <h1 class="admin-panel-subheader">
+                User Stats
+            </h1>
+            <ul>
+                <li><a href="#">PRS Callers Free Mins Stats</a></li>
+                <li><a href="#">Inactive Users</a></li>
+                <li><a href="#">Webcam Stats</a></li>
+                <li><a href="#">Management Stats</a></li>
+                <li><a href="#">Topup Stats</a></li>
+                <li><a href="#">Rampant VR Stats</a></li>
+                <li><a href="#">Roku Stats</a></li>
+                <li><a href="#">Lottery Stats</a></li>
+            </ul>
+        </div>
 
-            <div id="admin-panel-servers">
-                <h1 class="admin-panel-subheader">
-                    Servers
-                </h1>
-                <ul>
-                    <li><a href="#">MSioF</a></li>
-                    <li><a href="#">Pingdom</a></li>
-                    <li><a href="#">Portal</a></li>
-                </ul>
-            </div>
+        <div id="admin-panel-servers">
+            <h1 class="admin-panel-subheader">
+                Servers
+            </h1>
+            <ul>
+                <li><a href="#">MSioF</a></li>
+                <li><a href="#">Pingdom</a></li>
+                <li><a href="#">Portal</a></li>
+            </ul>
+        </div>
 
-            <div id="admin-panel-whitelist">
-                <h1 class="admin-panel-subheader">
-                    WhiteList
-                </h1>
-                <ul>
-                    <li><a href="#">Manage IPs</a></li>
-                </ul>
-            </div>
+        <div id="admin-panel-whitelist">
+            <h1 class="admin-panel-subheader">
+                WhiteList
+            </h1>
+            <ul>
+                <li><a href="#">Manage IPs</a></li>
+            </ul>
+        </div>
 
-            <div id="admin-panel-helpers">
-                <h1 class="admin-panel-subheader">
-                    Helpers
-                </h1>
-                <ul>
-                    <li><a href="#">Clear Forgotten Password IP Cache</a></li>
-                    <li><a href="#">Clear any Memcache Key</a></li>
-                    <li><a href="#">User Editor</a></li>
-                    <li><a href="#">Show Creator</a></li>
-                </ul>
-            </div>
+        <div id="admin-panel-helpers">
+            <h1 class="admin-panel-subheader">
+                Helpers
+            </h1>
+            <ul>
+                <li><a href="#">Clear Forgotten Password IP Cache</a></li>
+                <li><a href="#">Clear any Memcache Key</a></li>
+                <li><a href="#">User Editor</a></li>
+                <li><a href="#">Show Creator</a></li>
+            </ul>
+        </div>
 
-            <div id="admin-panel-force-default-stream">
-                <h1 class="admin-panel-subheader">
-                    Force Default Stream
-                </h1>
-                <form>
-                    <p>Force the default stream id to</p>
-                    <input type="number" name="from-value" min="1" max="99999">
-                    for
-                    <input type="number" name="to-value" min="1" max="99999">
-                    <button type="submit" value="submit">Set</button>
-                </form>
-            </div>
-            <div id="admin-panel-localhost-helpers">
-                <h1 class="admin-panel-subheader">Localhost Helpers </h1>
-                <ul>
-                    <li><a href="#">Concan/Min JS & CSS</a></li>
-                </ul>
-            </div>
+        <div id="admin-panel-force-default-stream">
+            <h1 class="admin-panel-subheader">
+                Force Default Stream
+            </h1>
+            <form>
+                <p>Force the default stream id to</p>
+                <input type="number" name="from-value" min="1" max="99999">
+                for
+                <input type="number" name="to-value" min="1" max="99999">
+                <button type="submit" value="submit">Set</button>
+            </form>
+        </div>
+        <div id="admin-panel-localhost-helpers">
+            <h1 class="admin-panel-subheader">Localhost Helpers </h1>
+            <ul>
+                <li><a href="#">Concan/Min JS & CSS</a></li>
+            </ul>
         </div>
     </div>
 </div>
-
+</div>
+-->
 
 <div id="admin-info-container" style="width:70%; height: 1500px; background-color: red; float:left;">
     <div id="main-user-details"
@@ -169,7 +250,8 @@
             </ul>
         </div>
     </div>
-    <div id="secondary-user-details" style="background-color:white; width:100%; height: 10%; float:left; margin: auto; display: inline-block;">
+    <div id="secondary-user-details"
+         style="background-color:white; width:100%; height: 10%; float:left; margin: auto; display: inline-block;">
         <div class="user-info-box">
             <h3 class="user-info-title">STATUS</h3>
             <p style="text-align: center;">ACTIVE</p>
@@ -187,8 +269,8 @@
             <p style="text-align: center;">dmclaren@firestormmedia.tv</p>
         </div>
         <div class="user-info-box">
-        <h3 class="user-info-title">DirtyChat ID</h3>
-        <p style="text-align: center;">483788</p>
+            <h3 class="user-info-title">DirtyChat ID</h3>
+            <p style="text-align: center;">483788</p>
         </div>
     </div>
 
