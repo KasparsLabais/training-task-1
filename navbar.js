ff3ec8b2ce99c73f5d52fba828e0ae6f2317012a
Danni <dmclaren@firestormmedia.tv>
2019-09-03 15:18:51 +0100

    added bootstrap

--- navbar.js
+++ navbar.js
@@ -13,3 +13,19 @@ function toggleNav(){
 function myFunction() {
     document.getElementsByClassName(".item-child li").classList.toggle("none");
   }
+
+
+  var dropdown = document.getElementsByClassName("item-father");
+  var i;
+  
+  for (i = 0; i < dropdown.length; i++) {
+    dropdown[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var dropdownContent = this.nextElementSibling;
+    if (dropdownContent.style.display === "block") {
+    dropdownContent.style.display = "none";
+    } else {
+    dropdownContent.style.display = "block";
+    }
+    });
+  }
